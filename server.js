2b2ada1fd4a98d4f5cfbc3ec92ea69663884b8fc
Ross F <rfahan@gmail.com>
2023-09-06 02:52:48 -0600

    added routes for index.html and notes.html

--- server.js
+++ server.js
@@ -7,13 +7,18 @@ const PORT = 3001;
 const app = express();
 
 app.use(express.static('public'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// Route for the notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  });
+
+  // Route for index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
